637f6ce1436e02ace52d11bf2d2d7dcf8a5487bc
Ittay Alfassi <ittay.alfassi@gmail.com>
2018-01-10 20:21:05 +0200

    finally made project work without anniversary edition. will now work in all VS versions.

--- risk client/risk project/Classes/Helper.cs
+++ risk client/risk project/Classes/Helper.cs
@@ -11,12 +11,13 @@ using Windows.UI.Xaml.Media;
 using Windows.UI;
 using Windows.Media.Playback;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
 
 namespace risk_project
 {
     static class Helper
     {
-        private static MediaPlayer player;
+        private static MediaElement player;
         public static bool musicPlaying;
         public static bool soundPlaying;
         public static bool fullScreen;
@@ -55,14 +56,14 @@ namespace risk_project
             view.ExitFullScreenMode();
         }
 
-        public static void InitMusic()
+        public static async void InitMusic()
         {
-            player = new MediaPlayer();
-            player.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/Data/music.mp3"));
-            if (musicPlaying)
-            {
-                player.Play();
-            }
+            player = new MediaElement();
+            StorageFolder Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            Folder = await Folder.GetFolderAsync(@"Assets\\Data");
+            StorageFile sf = await Folder.GetFileAsync("music.mp3");
+            player.SetSource(await sf.OpenAsync(FileAccessMode.Read), sf.ContentType);
+            player.Play();
         }
 
         public static bool IsMusicPlaying() { return musicPlaying; }
